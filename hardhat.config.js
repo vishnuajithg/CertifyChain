8fe7a8007256fa05c8a6cfb13cf00c0aa90ac731
VISHNU A G <69210756+vishnuajithg@users.noreply.github.com>
2024-09-06 14:23:23 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,8 +11,8 @@ module.exports = {
       url:"http://127.0.0.1:8545/"
     },
     infuraSep:{
-    	url:SEPOLIA_URL || "http://127.0.0.1:8545/",
-    	accounts:[PRIVATE_KEY] || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    	url:SEPOLIA_URL,
+    	accounts:[PRIVATE_KEY]
     },
   },
   solidity: "0.8.20",
